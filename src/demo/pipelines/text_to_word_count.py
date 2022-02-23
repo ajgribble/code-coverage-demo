76c6563abafb716b16178b1f013b4f27957d559e
Adam Gribble <only-one@adamgribble.com>
2022-02-22 19:37:37 -0500

    More tests

--- src/demo/pipelines/text_to_word_count.py
+++ src/demo/pipelines/text_to_word_count.py
@@ -1,6 +1,7 @@
 from demo.stages import Reduce, Map, Parse
 
-stop_words = ["a", "the", "is"]
+stop_words = ["a", "and", "the", "in", "is"]
+supported_file_exts = ["txt"]
 
 
 def _is_stop_word(word):
@@ -16,11 +17,16 @@ def run(file_path, filter_stop_words=False):
             file_path (str): Path on disk to the text file to parse
             filter_stop_words (bool): Flag, that when True, filters out known stop words (defaults to False)
     """
+
+    # We only want to support a subset of file types
+    if file_path.split(".")[-1] not in supported_file_exts:
+        raise ValueError("The file type provided is not supported")
+
     # Parse words from text
     words = Parse.text(file_path)
 
     if filter_stop_words:
-        words = filter(lambda w: not _is_stop_word(w))
+        words = filter(lambda w: not _is_stop_word(w), words)
 
     word_tuples = Map.to_count(words)
     result = Reduce.sum(word_tuples)
--- tests/test_text_to_word_count.py
+++ tests/test_text_to_word_count.py
@@ -1,12 +1,18 @@
-from demo.pipelines.text_to_word_count import run as run_text_to_word_count
+import pytest
+from demo.pipelines.text_to_word_count import (
+    _is_stop_word,
+    run as run_text_to_word_count,
+)
 
 base_path = "tests/sample_data/text"
+json_path = "tests/sample_data/json"
 
 
 def test_default_processing_valid_text():
     result = run_text_to_word_count(f"{base_path}/missing_poem.txt")
 
     assert result["hunted"] == 2
+    assert result["in"] == 2
     assert result["my"] == 4
     assert result["them"] == 2
 
@@ -17,3 +23,28 @@ def test_default_processing_alt_text():
     assert result["little"] == 2
     assert result["tinkle"] == 4
     assert result["you"] == 3
+
+
+def test_default_processing_valid_text_sans_stop_words():
+    result = run_text_to_word_count(
+        f"{base_path}/missing_poem.txt", filter_stop_words=True
+    )
+
+    assert result["hunted"] == 2
+    assert result["my"] == 4
+    assert result["them"] == 2
+    assert "in" not in result
+
+
+def test_is_stop_word():
+    """
+    This test is superfluous and may result in false positives that a block of code is actually run
+    """
+    assert _is_stop_word("the") is True
+    assert _is_stop_word("is") is True
+    assert _is_stop_word("something") is False
+
+
+def test_file_extension_exceptions():
+    with pytest.raises(ValueError):
+        run_text_to_word_count(f"{json_path}/poems.json")
